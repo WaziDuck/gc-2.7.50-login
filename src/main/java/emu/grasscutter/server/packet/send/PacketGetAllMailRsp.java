6ca4f39c1bf8a857785d7b413241ec3684ce9988
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-05-30 10:36:10 +0800

    feature(2.7.50+ version): upload new protos
    1. Upload new protos;
    2. Fix some bugs cuz by new protos;
    3. uint -> fixed;
    4. Allow WindSeed to implement Patch by sending Lua scripts.
    
    Bonus: We can't do anything, even though WindSeed or PlayerLua are blacklisted, the people who want to do bad things can still build a de-blacklisted version on their own.
    
    BREAKING CHANGE: ALL

--- src/main/java/emu/grasscutter/server/packet/send/PacketGetAllMailRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketGetAllMailRsp.java
@@ -24,9 +24,9 @@ public class PacketGetAllMailRsp extends BasePacket {
         GetAllMailRsp.Builder proto = GetAllMailRsp.newBuilder();
 
         if (isGiftMail) {
-            proto.setANKKGPJCINB(true);
+            proto.setCJPALFPMFFB(true);
         } else {
-            proto.setANKKGPJCINB(false);
+            proto.setCJPALFPMFFB(false);
 
             if (player.getAllMail().size() != 0) { // Make sure the player has mail
                 List<MailData> mailDataList = new ArrayList<MailData>();
@@ -61,7 +61,7 @@ public class PacketGetAllMailRsp extends BasePacket {
                                 mailData.setImportance(message.importance);
                                 mailData.setIsRead(message.isRead);
                                 mailData.setIsAttachmentGot(message.isAttachmentGot);
-                                mailData.setBHCAHLJIKFFValue(1);
+                                mailData.setConfigId(1); // Maybe this is the config id?
 
                                 mailDataList.add(mailData.build());
                             }
